1e82f8842a1bc0a3d7d44eda8105878cb82a9059
iostreamer <iostreamer007@gmail.com>
2015-04-13 18:10:03 +0530

    Usage ready

--- src/main/scala/labs/amethyst/Crave.scala
+++ src/main/scala/labs/amethyst/Crave.scala
@@ -11,8 +11,5 @@ import labs.amethyst.CraveCore._
 
 object Crave extends App {
     Terminal.initialize
-    var testData = List(9.0, 9.1, 9.2, 8.2, 10.11, 10.19, 10.42, 9.3, 8.4, 8.14)
     Terminal.?
-    testData.foreach(i => testData = testData.::(i + 12))
-    testData.foreach(bufferDataSet !)
 }
